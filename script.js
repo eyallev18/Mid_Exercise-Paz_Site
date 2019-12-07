9a931bd8eb7cc20f01be3ac3e91fa94a5faa7808
Eyal Lev <eyal18yaffa03@gmail.com>
2019-12-08 00:06:58 +0200

    Final_for_apply

--- script.js
+++ script.js
@@ -14,12 +14,7 @@ function submitForm() {
     updateGradeValue();
     
     
-    setInterval(function() {
-      
-      var currentValue = document.getElementById("progress").value;
-      document.getElementById("progress").value = currentValue + 1;
-      
-    }, 1000);
+    
   }
   function mouseOver() {
     document.getElementById("non_hover").className = "hover";
@@ -29,6 +24,34 @@ function submitForm() {
     document.getElementById("non_hover").className = "non_hover";
     document.getElementById("hover").className = "hover";
   }
+  function mouseOver_read_m1() {
+    document.getElementById("non_hover_read_m1").className = "read_more hover_read_m1";
+    document.getElementById("hover_read_m1").className = "read_more non_hover_read_m1";
+  }
+  function mouseOut_read_m1() {
+    document.getElementById("non_hover_read_m1").className = "read_more non_hover_read_m1";
+    document.getElementById("hover_read_m1").className = "read_more hover_read_m1";
+  }
+
+  var image=document.getElementById("first_swap_pic");
   
-  
-  
+  var image_tracker = 'Oz_pic';
+
+  function swap(){
+    var image = document.getElementById('first_swap_pic');
+    if(image_tracker=='Oz_pic'){
+    image.src='250X110668568400.jpg';
+    image_tracker='250X110668568400';
+    }
+    else 
+     if (image_tracker=='250X110668568400'){
+    image.src='banner_atar.jpg';
+    image_tracker='banner_atar';
+    }
+
+    else {
+        image.src='Oz_pic.JPG';
+        image_tracker='Oz_pic';
+        }
+    }
+    var timer = setInterval('swap();',2000); 
